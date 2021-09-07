ef20f5585c0adc148796f249e9d75ed070bae17b
Herman Benao <hermben@gmail.com>
2021-09-07 14:06:45 -0700

    adds logic to triangle.cs

--- QuiltPatterns/triangle.cs
+++ QuiltPatterns/triangle.cs
@@ -8,5 +8,24 @@ namespace QuiltPatterns
 {
     class triangle
     {
+        //member variables
+        internal double a;
+        internal double b;
+        internal double h;
+
+        double GetArea()
+        {
+            return 1 / 2 * (b*h);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", a);
+            Console.WriteLine("Width: {0}", b);
+            Console.WriteLine("Width: {0}", h);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+
+
     }
 }
